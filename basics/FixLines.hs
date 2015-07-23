5e5822d6d7adcc1ef70e0a511c66dff0ee9add5b
Dou Yutao <yutao.dou@rea-group.com>
2015-07-23 20:21:12 +0800

    Fix lines example

---
+++ basics/FixLines.hs
@@ -0,0 +1,28 @@
+import System.Environment (getArgs)
+
+splitLines [] = []
+splitLines cs =
+    let (pre, suf) = break isLineTerminate cs
+    in pre : case suf of
+        ('\r':'\n':rest) -> splitLines rest
+        ('\r':rest) -> splitLines rest
+        ('\n':rest) -> []
+        _ -> []
+
+isLineTerminate c = c =='\r' || c == '\n'
+
+fixLines :: String -> String
+fixLines input = unlines (splitLines input)
+
+interactWith function inputFile outputFile = do
+    input <- readFile inputFile
+    writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input, output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+
+        myFunction = fixLines
--- main.hs
+++ main.hs
@@ -1,2 +1,10 @@
-main :: IO ()
-main = do putChar 'a'
+splitLines [] = []
+splitLines cs =
+    let (pre, suf) = break isLineTerminate cs
+    in pre : case suf of
+        ('\r':'\n':rest) -> splitLines rest
+        ('\r':rest) -> splitLines rest
+        ('\n':rest) -> []
+
+isLineTerminate c = c =='\r' || c == '\n'
+
